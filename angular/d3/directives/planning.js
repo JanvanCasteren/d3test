9a2684e8684991ddce1e19d1b47854ac9e186b99
Jan van Casteren <jvancasteren@gmail.com>
2014-11-19 20:07:57 +0100

    tweede element d3-planning toegevoegd met verwijzing naar andere tabel

--- angular/d3/directives/planning.js
+++ angular/d3/directives/planning.js
@@ -3,12 +3,14 @@ angular.module('wijnemenjemee')
 /**
  * Width of svg element will adapt to the width of the parent container of the
  * directive element
+ * Note: I don't use isolated scope, so put the controller directive inside the
+ * tag of each d3-planning element if you use more of them on the same page
  */
  .directive('d3Planning', function () {
     return {
         restrict: 'E',
         template: '<div></div>',
-        link: function ($scope, $element) {
+        link: function (scope, $element) {
 
             var container = d3.select('div', $element);
            
@@ -21,7 +23,7 @@ angular.module('wijnemenjemee')
             //omdat de data asynchroon geladen wordt heb ik
             //een watch nodig, 
             //zie http://stackoverflow.com/questions/15112584/using-scope-watch-and-scope-apply
-            $scope.$watch('data', function (newData, oldData) {
+            scope.$watch('data', function (newData, oldData) {
                 if(newData)
                 {
                    var chart = d3.timeline()
